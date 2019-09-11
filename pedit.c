02272e1801da6034b94b5ff3de6e6ea03905437f
Pedro de Brito <ped.de.brito@gmail.com>
2019-09-11 18:18:58 +0300

    Use control sequences to hide the cursor during screen refresh
    ?25l sets the cursor as invisible
    ?25h resets the cursor as visible
    
    https://vt100.net/docs/vt510-rm/DECTCEM.html

--- pedit.c
+++ pedit.c
@@ -122,12 +122,14 @@ void editorDrawRows(struct abuf *ab) {
 void editorRefreshScreen() {
     struct abuf ab = ABUF_INIT;
 
+    abAppend(&ab, "\x1b[?25l", 4);
     abAppend(&ab, "\x1b[2J", 4);
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
 
     abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h", 4);
 
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
