33b080994f69f5dcd448d14f0dd8e816e6d3cee8
Pedro de Brito <ped.de.brito@gmail.com>
2019-09-21 21:01:04 +0300

    Limit scrolling to the right
    Use size member of E.row[E.cy] to limit the maximum value of E.cx

--- pedit.c
+++ pedit.c
@@ -272,6 +272,8 @@ void editorRefreshScreen() {
 }
 
 void editorMoveCursor(int key) {
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
     switch (key) {
     case ARROW_LEFT:
         if (E.cx > 0) {
@@ -279,7 +281,10 @@ void editorMoveCursor(int key) {
         }
         break;
     case ARROW_RIGHT:
+        if (row && E.cx < row->size )
+        {
             E.cx++;
+        }
         break;
     case ARROW_DOWN:
         if (E.cy < E.numrows) {
