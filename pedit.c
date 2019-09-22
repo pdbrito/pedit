301be0717dcdd20b2e1ccf96c7524543d4303436
Pedro de Brito <ped.de.brito@gmail.com>
2019-09-22 11:48:50 +0300

    Snap cursor to end of line
    After processing a move command redeclare row to get length again
    The cursor could have moved up/down from previouw row declaration
    If row is not null extract its size into rowlen
    If E.cx > rowlen set E.cx = rowlen

--- pedit.c
+++ pedit.c
@@ -281,8 +281,7 @@ void editorMoveCursor(int key) {
         }
         break;
     case ARROW_RIGHT:
-        if (row && E.cx < row->size )
-        {
+        if (row && E.cx < row->size ) {
             E.cx++;
         }
         break;
@@ -296,6 +295,12 @@ void editorMoveCursor(int key) {
             E.cy--;
         }
         break;
+    }
+
+    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    int rowlen = row ? row->size : 0;
+    if (E.cx > rowlen) {
+        E.cx = rowlen;
     }
 }
 
