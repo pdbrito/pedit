e96a8e9adf86fe874b8319431c1e42f8aa1ecb33
Pedro de Brito <ped.de.brito@gmail.com>
2019-09-26 20:43:51 +0300

    Show current line number in status bar, aligned to the right

--- pedit.c
+++ pedit.c
@@ -302,14 +302,21 @@ void editorDrawRows(struct abuf *ab) {
 
 void editorDrawStatusBar(struct abuf *ab) {
     abAppend(ab, "\x1b[7m", 4);
-    char status[80];
+    char status[80], rstatus[80];
     int len = snprintf(status, sizeof(status), "%.20s - %d lines",
         E.filename ? E.filename : "[No Name]", E.numrows);
+    int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d",
+        E.cy + 1, E.numrows);
     if (len > E.screenCols) len = E.screenCols;
     abAppend(ab, status, len);
     while (len < E.screenCols) {
-        abAppend(ab, " ", 1);
-        len++;
+        if (E.screenCols - len == rlen) {
+            abAppend(ab, rstatus, rlen);
+            break;
+        } else {
+            abAppend(ab, " ", 1);
+            len++;
+        }
     }
     abAppend(ab, "\x1b[m", 3);
 }
