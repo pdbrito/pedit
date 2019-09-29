cc9e2e6321852351c397904558c5b01fe2451d2d
Pedro de Brito <ped.de.brito@gmail.com>
2019-09-29 12:54:03 +0300

    Prevent insertion of special characters
    Add case statements that break when common special characters are input
    '\r' is the enter key
    CTRL+H is control code 8, originally used for backspace
    CTRL+L is used to refresh screen on input, we already do this
    '\x1b' is escape. Ignore it to prevent registering unhandled sequences

--- pedit.c
+++ pedit.c
@@ -15,6 +15,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
+    BACKSPACE = 127,
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
@@ -422,6 +423,9 @@ void editorProcessKeypress() {
     int c = editorReadKey();
 
     switch (c) {
+    case '\r':
+        break;
+
     case CTRL_KEY('q'):
         write(STDOUT_FILENO, "\x1b[2J", 4);
         write(STDOUT_FILENO, "\x1b[H", 3);
@@ -435,6 +439,11 @@ void editorProcessKeypress() {
     case END_KEY:
         if (E.cy < E.numrows)
             E.cx = E.row[E.cy].size;
+        break;
+
+    case BACKSPACE:
+    case CTRL_KEY('h'):
+    case DEL_KEY:
         break;
 
     case PAGE_UP:
@@ -458,6 +467,10 @@ void editorProcessKeypress() {
     case ARROW_LEFT:
     case ARROW_RIGHT:
         editorMoveCursor(c);
+        break;
+
+    case CTRL_KEY('l'):
+    case '\x1b':
         break;
 
     default:
