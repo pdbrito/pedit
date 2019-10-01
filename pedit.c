146b6d18ab38897b25bf2914da10bf7eb1304786
Pedro de Brito <ped.de.brito@gmail.com>
2019-10-01 18:29:13 +0300

    Reset E.dirty to 0 after file open or file save

--- pedit.c
+++ pedit.c
@@ -271,6 +271,7 @@ void editorOpen(char *filename) {
     }
     free(line);
     fclose(fp);
+    E.dirty = 0;
 }
 
 void editorSave() {
@@ -285,6 +286,7 @@ void editorSave() {
             if (write(fd, buf, len) == len) {
                     close(fd);
                     free(buf);
+                    E.dirty = 0;
                     editorSetStatusMessage("%d bytes written to disk", len);
                     return;
             }
