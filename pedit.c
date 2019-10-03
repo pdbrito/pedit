ff865ef0e540a9bf9e31ce808ccab63956d331bd
Pedro de Brito <ped.de.brito@gmail.com>
2019-10-03 20:55:58 +0300

    Add delete and backspace functionality
    If DEL_KEY is pressed, move the cursor one right and then delete left
    Otherwise just delete left

--- pedit.c
+++ pedit.c
@@ -525,6 +525,8 @@ void editorProcessKeypress() {
     case BACKSPACE:
     case CTRL_KEY('h'):
     case DEL_KEY:
+        if (c == DEL_KEY) editorMoveCursor(ARROW_RIGHT);
+        editorDelChar();
         break;
 
     case PAGE_UP:
