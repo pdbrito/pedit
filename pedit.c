ae7f9340f635b25fabb8790572478c4a88f42b93
Pedro de Brito <ped.de.brito@gmail.com>
2019-10-04 18:11:56 +0300

    Add editorFreeRow()
    Frees the dynamic memory in use by the passed in row

--- pedit.c
+++ pedit.c
@@ -217,6 +217,11 @@ void editorAppendRow(char *s, size_t len) {
     E.dirty++;
 }
 
+void editorFreeRow(erow *row) {
+    free(row->render);
+    free(row->chars);
+}
+
 void editorRowInsertChar(erow *row, int at, int c) {
     if (at < 0 || at > row->size) at = row->size;
     row->chars = realloc(row->chars, row->size + 2);
