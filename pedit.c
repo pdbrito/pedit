f2ce42238c65bec2886194503efea229f13f0089
Pedro de Brito <ped.de.brito@gmail.com>
2019-10-10 21:02:28 +0300

    Trigger editorFind() with Ctrl-F

--- pedit.c
+++ pedit.c
@@ -642,6 +642,10 @@ void editorProcessKeypress() {
             E.cx = E.row[E.cy].size;
         break;
 
+    case CTRL_KEY('f'):
+        editorFind();
+        break;
+
     case BACKSPACE:
     case CTRL_KEY('h'):
     case DEL_KEY:
@@ -710,7 +714,8 @@ int main(int argc, char *argv[]) {
         editorOpen(argv[1]);
     }
 
-    editorSetStatusMessage("HELP: Ctrsl-S = save | Ctrl-Q = quit");
+    editorSetStatusMessage(
+        "HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
 
     while (1) {
         editorRefreshScreen();
