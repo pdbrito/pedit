42105de2d92aecda6b8f2faadc69b5df732e88bb
Pedro de Brito <ped.de.brito@gmail.com>
2019-10-15 18:55:59 +0300

    Add case to editorSyntaxToColor for HL_MATCH
    Return 34, which sets foreground blue

--- pedit.c
+++ pedit.c
@@ -191,6 +191,7 @@ void editorUpdateSyntax(erow *row) {
 int editorSyntaxToColor(int hl) {
     switch (hl) {
         case HL_NUMBER: return 31;
+        case HL_MATCH: return 34;
         default: return 37;
     }
 }
