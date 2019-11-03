f81d95f584373a875add7a277a6ed6e030112593
Pedro de Brito <ped.de.brito@gmail.com>
2019-11-03 21:50:44 +0100

    Add keywords array to editorSyntax struct
    NULL terimanted array of strings
    Each string is a keyword
    To differentiate between types, terminate second type of keyword with |

--- pedit.c
+++ pedit.c
@@ -46,16 +46,25 @@ enum editorHighlight {
 struct editorSyntax {
     char *filetype;
     char **filematch;
+    char **keywords;
     char *singleline_comment_start;
     int flags;
 };
 
 char *C_HL_extensions[] = { ".c", ".h", ".cpp"};
+char *C_HL_keywords[] = {
+    "switch", "if", "while", "for", "break", "continue", "return", "else",
+    "struct", "union", "typedef", "static", "enum", "class", "case",
+
+    "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
+    "void|", NULL
+};
 
 struct editorSyntax HLDB[] = {
     {
         "c",
         C_HL_extensions,
+        C_HL_keywords,
         "//",
         HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
     }
