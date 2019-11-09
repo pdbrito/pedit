e38e6db8db286ec890464fe8b78351efe3ffb621
Pedro de Brito <ped.de.brito@gmail.com>
2019-11-09 23:39:41 +0100

    Update editorSyntax with members for multiline comment start and end

--- pedit.c
+++ pedit.c
@@ -49,6 +49,8 @@ struct editorSyntax {
     char **filematch;
     char **keywords;
     char *singleline_comment_start;
+    char *multiline_comment_start;
+    char *multiline_comment_end;
     int flags;
 };
 
@@ -66,7 +68,7 @@ struct editorSyntax HLDB[] = {
         "c",
         C_HL_extensions,
         C_HL_keywords,
-        "//",
+        "//", "/*", "*/",
         HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
     }
 };
